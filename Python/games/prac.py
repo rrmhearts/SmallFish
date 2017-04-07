ee48a3ce3796e72860d8bb1cc70d240996befec2
Ryan McCoppin <rrmhearts@gmail.com>
2017-04-07 16:43:59 -0400

    random additions

---
+++ Python/games/prac.py
@@ -0,0 +1,24 @@
+
+
+
+print "The Galaxy of TWARS"
+
+#print "What is your name?"
+
+#name = ("What is your name?")
+
+#print "your name is ", name
+
+lnames = ["Roolo Soldier", "Kay II", "Lenno", "Ra-ven Leader", "Roolo Guard", "Lightning King"]
+
+
+for name in lnames:
+    if name == "Lenno":
+        print "I am ", name
+    else:
+        print "I am the ", name
+        
+print "We must attack These!"
+
+bnames = [ "Bone", "Maxx", "Alien Soldier", "Alien Leader", "Aklakay", "Gor ila"]
+
